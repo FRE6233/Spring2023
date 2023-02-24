e70d206738bf638fd7e1d0088581d2b99faa1573
Keith A. Lewis <kal12@nyu.edu>
2023-02-24 02:40:09 +0000

    hw4

--- cpp/fms_distribution_double_exponential.h
+++ cpp/fms_distribution_double_exponential.h
@@ -10,23 +10,24 @@ namespace fms::distribution {
 
 	template<class X = double, class S = X>
 	struct double_exponential : public standard<X, S> {
-		static constexpr X beta = M_SQRT2;
+		static constexpr X β = M_SQRT2;
 
 		// e^{sz - κ(s)} f(z)
 		X _pdf(const X& z, const S& s) const override
 		{
-			return 0*z*s; //!!! implement
+			return exp(s * z - _cgf(s)) * exp(-β * fabs(z)) / β;
 		}
 		// (1 − s/β)e^{(s + β)x}/2, x <= 0
 		// 1 − (1 + s/β)e^{(s − β)x}/2, x >= 0
 		X _cdf(const X& z, const S& s) const override
 		{
-			return 0*z*s; //!!! implement
+			return z <= 0 ? (1 - s / β) * exp((s + β) * z) / 2 : 1 - (1 + s / β) * exp((s - β) * z) / 2;
 		}
+
 		// E[e^{sX}] = β^2/(β^2 - s^2), s < β.
 		X _mgf(const S& s) const override
 		{
-			return 0*s; //!!! implement
+			return β * β / (β * β - s * s);
 		}
 		X _cgf(const S& s) const override
 		{
