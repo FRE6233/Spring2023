9a7ea33e3cf2fbcfb6d78b5440894720604187a5
Keith A. Lewis <kal12@nyu.edu>
2023-04-07 00:32:42 +0000

    bootstrap

--- xai/xll_bootstrap.cpp
+++ xai/xll_bootstrap.cpp
@@ -53,36 +53,121 @@ AddIn xai_pwflat_value(
 _FPX* WINAPI xll_pwflat_value(HANDLEX curve, _FPX* pt)
 {
 #pragma XLLEXPORT
-	//!!! Add try-catch for exceptions.
-	handle<pwflat::curve<>> c(curve);
-	// ensure(c);
-	for (unsigned i = 0; i < size(*pt); ++i) {
-		pt->array[i] = c->value(pt->array[i]);
+	try {
+		handle<pwflat::curve<>> c(curve);
+		ensure(c);
+		for (unsigned i = 0; i < size(*pt); ++i) {
+			pt->array[i] = c->value(pt->array[i]);
+		}
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+		return 0;
 	}
 
 	return pt;
 }
 
-//!!! Implement XLL.CURVE.DISCOUNT and XLL.CURVE.SPOT
-//!!! Add discount and spot to bootstrap.xlsx
+AddIn xai_pwflat_spot(
+	Function(XLL_FPX, "xll_pwflat_spot", "XLL.CURVE.SPOT")
+	.Arguments({
+		Arg(XLL_HANDLEX, "curve", "is handle returned by \\XLL.CURVE."),
+		Arg(XLL_FPX, "time", "is an array of times."),
+		})
+		.Category(CATEGORY)
+	.FunctionHelp("Return spot rate of flat curve at given times.")
+);
+_FPX* WINAPI xll_pwflat_spot(HANDLEX curve, _FPX* pt)
+{
+#pragma XLLEXPORT
+	try {
+		handle<pwflat::curve<>> c(curve);
+		ensure(c);
+		for (unsigned i = 0; i < size(*pt); ++i) {
+			pt->array[i] = c->spot(pt->array[i]);
+		}
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+		return 0;
+	}
 
-//!!! Implement \XLL.FIXED_INCOME.INSTRUMENT
+	return pt;
+}
+
+AddIn xai_pwflat_discount(
+	Function(XLL_FPX, "xll_pwflat_discount", "XLL.CURVE.DISCOUNT")
+	.Arguments({
+		Arg(XLL_HANDLEX, "curve", "is handle returned by \\XLL.CURVE."),
+		Arg(XLL_FPX, "time", "is an array of times."),
+		})
+		.Category(CATEGORY)
+	.FunctionHelp("Return discount of flat curve at given times.")
+);
+_FPX* WINAPI xll_pwflat_discount(HANDLEX curve, _FPX* pt)
+{
+#pragma XLLEXPORT
+	try {
+		handle<pwflat::curve<>> c(curve);
+		ensure(c);
+		for (unsigned i = 0; i < size(*pt); ++i) {
+			pt->array[i] = c->discount(pt->array[i]);
+		}
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+		return 0;
+	}
+
+	return pt;
+}
+
 AddIn xai_fixed_income_instrument_(
 	Function(XLL_HANDLEX, "xll_fixed_income_instrument_", "\\XLL.FIXED_INCOME.INSTRUMENT")
 	.Arguments({
-		//!!! Arg time
-		//!!! Arg cash
+		Arg(XLL_FPX, "time", "is an array of times."),
+		Arg(XLL_FPX, "cash", "is an array of cash flows."),
 		})
-	.Uncalced()
+		.Uncalced()
 	.Category(CATEGORY)
-	.FunctionHelp("Return a handles to times and cash flows of a fixed income instrument.")
+	.FunctionHelp("Return a handle to a piece-wise flat curve.")
 );
 HANDLEX WINAPI xll_fixed_income_instrument_(const _FPX* pu, const _FPX* pc)
 {
 #pragma XLLEXPORT
-	HANDLEX h = INVALID_HANDLEX;
+	HANDLEX h = INVALID_HANDLEX; // default return value
 
-	//!!!hint: handle<fixed_income::instrument<>> i(new fixed_income::instrument_value(size(*pu), pu->array, pc->array);
+	try {
+		ensure(size(*pu) == size(*pc));
+
+		handle<fixed_income::instrument<>> i(new fixed_income::instrument_value(size(*pu), pu->array, pc->array));
+		ensure(i);
+
+		h = i.get();
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+	}
 
 	return h;
+}
+
+AddIn xai_bootstrap_extend(
+	Function(XLL_HANDLE, "xll_bootstrap_extend", "BOOTSTRAP.EXTEND")
+	.Arguments({
+		Arg(XLL_HANDLE, "inst", "instrument"),
+		Arg(XLL_HANDLE, "curve", "curve"),
+		})
+		.Category(CATEGORY)
+	.FunctionHelp("Extend curve by instrument.")
+);
+HANDLEX WINAPI xll_bootstrap_extend(HANDLEX inst, HANDLEX curve)
+{
+#pragma XLLEXPORT
+	handle<fixed_income::instrument<>> i(inst);
+	handle<pwflat::curve<>> c(curve);
+
+	bootstrap::extend(*i, *c);
+
+	return curve;
 }
